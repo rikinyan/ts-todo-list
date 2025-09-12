545d9b17620505dccf01d081d63afd7f1398b6a1
rikiishi yumu <yumu@rikiishinoMacBook-Pro.local>
2025-09-13 01:18:27 +0900

    add todo&todolist domain test

---
+++ test/domain/Todo.test.ts
@@ -0,0 +1,29 @@
+import {describe} from "mocha"
+import Todo from "../../src/domain/Todo"
+import { assert } from "chai"
+
+describe("Todo displayString", () => {
+    const todo = new Todo(
+        "1",
+        "test todo",
+        false,
+        new Date()
+    )
+
+    assert.equal(
+        todo.displaystring(),
+        "[ ] 1: test todo"
+    )
+
+    const endTodo = new Todo(
+        "1",
+        "test todo",
+        true,
+        new Date()
+    )
+
+    assert.equal(
+        endTodo.displaystring(),
+        "[x] 1: test todo"
+    )
+})
