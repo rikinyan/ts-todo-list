070237029845665f17de38b3c3658c01694122d3
rikiishi yumu <yumu@rikiishinoMacBook-Pro.local>
2025-12-05 00:11:39 +0900

    fix test

--- test/commands/remove.test.ts
+++ test/commands/remove.test.ts
@@ -1,14 +1,32 @@
 import {runCommand} from '@oclif/test'
+import {sharedDbConnection} from "../../src/db/ConnectDatabase.js"
 import {expect} from 'chai'
 
 describe('remove', () => {
-  it('runs remove cmd', async () => {
-    const {stdout} = await runCommand('remove')
-    expect(stdout).to.contain('hello world')
+  beforeEach(async () => {
+    await sharedDbConnection.deleteFrom("todos").execute()
   })
 
-  it('runs remove --name oclif', async () => {
-    const {stdout} = await runCommand('remove --name oclif')
-    expect(stdout).to.contain('hello oclif')
+  it('runs remove cmd', async () => {
+    await runCommand('add "todo1"')
+    await runCommand('add "todo2"')
+
+    const results = await sharedDbConnection
+      .selectFrom("todos")
+      .selectAll()
+      .execute()
+
+    expect(results).to.have.length(2)
+
+    const deletedId = results[0].id.toString()
+
+    await runCommand(`remove ${deletedId}`)
+
+    const afterDeleted = await sharedDbConnection
+      .selectFrom("todos")
+      .selectAll()
+      .execute()
+
+    expect(afterDeleted).to.have.length(1)
   })
 })
--- test/commands/update.test.ts
+++ test/commands/update.test.ts
@@ -1,7 +1,12 @@
 import {runCommand} from '@oclif/test'
 import {expect} from 'chai'
+import {sharedDbConnection} from "../../src/db/ConnectDatabase.js"
 
 describe('update', () => {
+  beforeEach(async () => {
+    await sharedDbConnection.deleteFrom("todos").execute()
+  })
+
   it('runs update cmd', async () => {
     const {stdout} = await runCommand('update')
     expect(stdout).to.contain('hello world')
