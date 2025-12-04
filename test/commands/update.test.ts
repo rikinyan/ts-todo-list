8e177fba598c3f1460509db580051a94e1a525ea
rikiishi yumu <yumu@rikiishinoMacBook-Pro.local>
2025-12-05 01:27:42 +0900

    fix

--- test/commands/update.test.ts
+++ test/commands/update.test.ts
@@ -1,19 +1,19 @@
-import { runCommand } from "@oclif/test";
-import { expect } from "chai";
-import { sharedDbConnection } from "../../src/db/ConnectDatabase.js";
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import {sharedDbConnection} from "../../src/db/ConnectDatabase.js"
 
-describe("update", () => {
+describe('update', () => {
   beforeEach(async () => {
-    await sharedDbConnection.deleteFrom("todos").execute();
-  });
+    await sharedDbConnection.deleteFrom("todos").execute()
+  })
 
-  it("runs update cmd", async () => {
-    const { stdout } = await runCommand("update");
-    expect(stdout).to.contain("hello world");
-  });
+  it('runs update cmd', async () => {
+    const {stdout} = await runCommand('update')
+    expect(stdout).to.contain('hello world')
+  })
 
-  it("runs update --name oclif", async () => {
-    const { stdout } = await runCommand("update --name oclif");
-    expect(stdout).to.contain("hello oclif");
-  });
-});
+  it('runs update --name oclif', async () => {
+    const {stdout} = await runCommand('update --name oclif')
+    expect(stdout).to.contain('hello oclif')
+  })
+})
